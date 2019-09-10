a9091ec3506c5f0afe97f122f7df8170302fb175
sunxin <sunxin@taagoo.com>
2019-09-10 15:41:20 +0800

    2019-09-10

--- models/question.go
+++ models/question.go
@@ -28,7 +28,7 @@ func getId()(err error) {
 	db,err := LinkDb()
 
 	if err == nil {
-		rows,err := db.Query("SELECT id FROM quesiton_063")
+		rows,err := db.Query("SELECT id FROM question_063")
 		if err == nil{
 			for rows.Next(){
 				var id int
@@ -115,7 +115,7 @@ func InsertSingleQuestion(question comm.Question)(err error)  {
 			if err == nil {
 				_, err := stmt.Exec(question.Question,question.OptionA,question.OptionB,question.OptionC,question.OptionD,question.OptionE,question.OptionF,question.OptionG,question.CorrectAnswer1,question.CorrectAnswer2,question.CorrectAnswer3,question.CorrectAnswer4,question.CorrectAnswer5,question.CorrectAnswer6,question.CorrectAnswer7)
 				if err == nil {
-					//不是很清楚这个数字代表队的是什么，暂时认为可以返回这个值，代表是第几道题插入成功
+					//不是很清楚这个数字代表的是什么，暂时认为可以返回这个值，代表是第几道题插入成功
 					err = tx.Commit()
 				}
 			}
@@ -124,74 +124,107 @@ func InsertSingleQuestion(question comm.Question)(err error)  {
 	return err
 }
 
-func UpdateSingleQuestion(newquestion *comm.Question,oldquestion *comm.Question)(err error)  {
+//更新题库中指定的一条题库内容
+func UpdateSingleQuestion(newquestion *comm.Question,oldquestion *comm.Question,id int)(err error)  {
 
 	m := map[string]string{}
 
 	db,err := LinkDb()
 	if err == nil {
-		if newquestion.Question != oldquestion.Question {
-			m["Question"] = newquestion.Question
-		}
-		///这是优化代码，没有试验不太干确定
-		//s := [7]string{"A","B","C","D","E","F","G"}
-		//for i:=0;i<len(s);i++ {
-		//	option := "Option"+s[i]
-		//	newOption := "newquestion.Option"+ s[i]
-		//	oldOption := "oldquestion.Option"+ s[i]
-		//
-		//	if  newOption != oldOption{
-		//		m[option] = newOption
-		//	}
-		//}
-		///这段代码和上面的代码功能一样
-		if newquestion.OptionA != oldquestion.OptionA {
-			m["OptionA"] = newquestion.OptionA
-		}
-		if newquestion.OptionB != oldquestion.OptionB {
-			m["OptionB"] = newquestion.OptionB
-		}
-		if newquestion.OptionC != oldquestion.OptionC {
-			m["OptionC"] = newquestion.OptionC
-		}
-		if newquestion.OptionD != oldquestion.OptionD {
-			m["OptionD"] = newquestion.OptionD
-		}
-		if newquestion.OptionE != oldquestion.OptionE {
-			m["OptionE"] = newquestion.OptionE
-		}
-		if newquestion.OptionF != oldquestion.OptionF {
-			m["OptionF"] = newquestion.OptionF
-		}
-		if newquestion.OptionG != oldquestion.OptionG {
-			m["OptionG"] = newquestion.OptionG
-		}
+		tx,err := db.Begin()
+		if err == nil {
+			if newquestion.Question != oldquestion.Question {
+				m["Question"] = newquestion.Question
+			}
+			///这是优化代码，没有试验不太感确定
+			//s := [7]string{"A","B","C","D","E","F","G"}
+			//for i:=0;i<len(s);i++ {
+			//	option := "Option"+s[i]
+			//	newOption := "newquestion.Option"+ s[i]
+			//	oldOption := "oldquestion.Option"+ s[i]
+			//
+			//	if  newOption != oldOption{
+			//		m[option] = newOption
+			//	}
+			//}
+			///这段代码和上面的代码功能一样
+			if newquestion.OptionA != oldquestion.OptionA {
+				m["OptionA"] = newquestion.OptionA
+			}
+			if newquestion.OptionB != oldquestion.OptionB {
+				m["OptionB"] = newquestion.OptionB
+			}
+			if newquestion.OptionC != oldquestion.OptionC {
+				m["OptionC"] = newquestion.OptionC
+			}
+			if newquestion.OptionD != oldquestion.OptionD {
+				m["OptionD"] = newquestion.OptionD
+			}
+			if newquestion.OptionE != oldquestion.OptionE {
+				m["OptionE"] = newquestion.OptionE
+			}
+			if newquestion.OptionF != oldquestion.OptionF {
+				m["OptionF"] = newquestion.OptionF
+			}
+			if newquestion.OptionG != oldquestion.OptionG {
+				m["OptionG"] = newquestion.OptionG
+			}
 
-		if newquestion.CorrectAnswer1 != oldquestion.CorrectAnswer1 {
-			m["CorrectAnswer1"] = newquestion.CorrectAnswer1
-		}
-		if newquestion.CorrectAnswer2 != oldquestion.CorrectAnswer2 {
-			m["CorrectAnswer2"] = newquestion.CorrectAnswer2
-		}
-		if newquestion.CorrectAnswer3 != oldquestion.CorrectAnswer3 {
-			m["CorrectAnswer3"] = newquestion.CorrectAnswer3
-		}
-		if newquestion.CorrectAnswer4 != oldquestion.CorrectAnswer4 {
-			m["CorrectAnswer4"] = newquestion.CorrectAnswer4
-		}
-		if newquestion.CorrectAnswer5 != oldquestion.CorrectAnswer5 {
-			m["CorrectAnswer5"] = newquestion.CorrectAnswer5
-		}
-		if newquestion.CorrectAnswer6 != oldquestion.CorrectAnswer6 {
-			m["CorrectAnswer6"] = newquestion.CorrectAnswer6
-		}
-		if newquestion.CorrectAnswer7 != oldquestion.CorrectAnswer7 {
-			m["CorrectAnswer7"] = newquestion.CorrectAnswer7
-		}
+			if newquestion.CorrectAnswer1 != oldquestion.CorrectAnswer1 {
+				m["CorrectAnswer1"] = newquestion.CorrectAnswer1
+			}
+			if newquestion.CorrectAnswer2 != oldquestion.CorrectAnswer2 {
+				m["CorrectAnswer2"] = newquestion.CorrectAnswer2
+			}
+			if newquestion.CorrectAnswer3 != oldquestion.CorrectAnswer3 {
+				m["CorrectAnswer3"] = newquestion.CorrectAnswer3
+			}
+			if newquestion.CorrectAnswer4 != oldquestion.CorrectAnswer4 {
+				m["CorrectAnswer4"] = newquestion.CorrectAnswer4
+			}
+			if newquestion.CorrectAnswer5 != oldquestion.CorrectAnswer5 {
+				m["CorrectAnswer5"] = newquestion.CorrectAnswer5
+			}
+			if newquestion.CorrectAnswer6 != oldquestion.CorrectAnswer6 {
+				m["CorrectAnswer6"] = newquestion.CorrectAnswer6
+			}
+			if newquestion.CorrectAnswer7 != oldquestion.CorrectAnswer7 {
+				m["CorrectAnswer7"] = newquestion.CorrectAnswer7
+			}
 
-		
+			update := "update question_063 set "
+			column := ""
+			for key,value := range m {
+				column = column + key + "=" + value + " "
+			}
+			update = update + column + "where id=?"
 
+			stmt,err := db.Prepare(update)
+			if err == nil {
+				_,err = stmt.Exec(id)
+				if err == nil {
+					err = tx.Commit()
+				}
+			}
+		}
 	}
+	return err
+}
 
+//删除指定考题
+func DeleteSingleQuestion(id int)(err error)  {
+	db,err := LinkDb()
+	if err == nil {
+		tx,err := db.Begin()
+		if err == nil {
+			stmt,err := db.Prepare("DELETE FROM question_063 WHERE id=?")
+			if err == nil {
+				_,err := stmt.Exec(id)
+				if err == nil {
+					err = tx.Commit()
+				}
+			}
+		}
+	}
 	return err
 }
